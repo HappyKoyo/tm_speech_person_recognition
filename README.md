02079df3a070231e56336aec744bc47b8b1998cd
demulab <tenkose@hotmail.com>
2018-06-08 15:48:08 +0900

    fix in code review

---
+++ README.md
@@ -0,0 +1,10 @@
+# Speech Person Recognition 2018
+## Writter
+1. Enomoto
+2. Makino
+3. Okano
+## Memo 
+activate:
+    crowd_list_req_pub.py
+~~~
+~~~
--- scripts/spr.py
+++ scripts/spr.py
@@ -21,6 +21,7 @@ class SpeechAndPersonRecognition:
         self.crowd_list = []
         self.male_count = -1
         self.female_count = -1
+
         
     def getCrowdSizeCB(self,result):
         self.crowd_list = result.data.split(' ')
@@ -28,9 +29,11 @@ class SpeechAndPersonRecognition:
         self.male_count = self.crowd_list.count('male')
         self.female_count = self.crowd_list.count('female')
 
+
     def recogVoiceCB(self,sentence):
         self.riddle_req_pub.Publish(sentence)
         print "send riddle request."
+
         
     def rotateBase(self,angle):
         rotate_cmd = Twist()
@@ -40,11 +43,13 @@ class SpeechAndPersonRecognition:
             print rotate_cmd.angular.z
             self.cmd_vel_pub.publish(rotate_cmd)
             rospy.sleep(0.035)
+
             
     def speak(self,sentence):
-        #voice_cmd = '/usr/bin/picospeaker %s' %sentence
-        #subprocess.call(voice_cmd.strip().split(' '))
-        print sentence
+        voice_cmd = '/usr/bin/picospeaker %s' %sentence
+        subprocess.call(voice_cmd.strip().split(' '))
+        print [PICO] + sentence
+
 
     def startSPR(self):#-----------------state 0
         print 'state : 0'
@@ -52,53 +57,61 @@ class SpeechAndPersonRecognition:
         head_angle = Float64()
         head_angle.data = -0.4
         self.head_angle_pub.publish(head_angle)
-        rospy.sleep(2.0)
+        rospy.sleep(2.0) # move time
         #start riddle game
         self.speak("I want to play riddle game!")
         rospy.sleep(1)#wait 10 seconds
-        self.rotateBase(100)
+        self.rotateBase(100) # rotateBase(): 
         rospy.sleep(1)
         self.crowd_list_req_pub.publish(True)    
         return 1 #next state
 
+
     def stateSizeOfTheCrowd(self):#------state 1
+        '''
+        '''
         print 'state : 1'
-        if len(self.crowd_list) == 0:
+        if len(self.crowd_list) == 0: # tuusinn yabai?
             print "wait for response from object recognizer"
             return 1 #this state
         #state number of male
-        if self.female_count == 1:
-            speak("There is " + str(self.male_count) + ' male.')
+        if self.male_count == 0:
+            speak('There is not male.')
+        elif self.male_count == 1:
+            speak('There is ' + str(self.male_count) + ' male.')
         else:
-            speak("There are " + str(self.male_count) + ' males.')
+            speak('There are ' + str(self.male_count) + ' males.')
         rospy.sleep(1)
         speak("and ")
         rospy.sleep(1)
         #state number of female
-        if self.female_count == 1:
-            speak("There is " + str(self.female_count) + ' female.')
+        if self.female_count == 0:
+            speak('There is not female.')
+        elif self.female_count == 1:
+            speak('There is ' + str(self.female_count) + ' female.')
         else:
-            speak("There are " + str(self.female_count) + ' females.')
+            speak('There are ' + str(self.female_count) + ' females.')
         #state sum of crowd
         speak("There are "+str(self.male_count+self.female_count)+" people in total.")
         rospy.sleep(2.0)
         speak("Who want to play riddles with me?")
         rospy.sleep(3.0)
-        self.speech_req_pub.Publish(True)
+        self.speech_req_pub.Publish(True) # start google speech api stream
         
         return 2 #next state
+
 
     def playRiddleGame(self):#----------state 2
         print 'state : 2'
         
-        
         return 2 #this state
+
 
     def startBlindMansBluffGame(self):#--state 3
         print 'state : 3'
 
-
         return 4
+
 
     def leaveArena(self):#---------------state 4
         print 'state : 4'
@@ -113,7 +126,7 @@ if __name__ == '__main__':
     rospy.init_node('speech_and_person_recognition')
     spr = SpeechAndPersonRecognition()
     main_state = 0
-    while main_state < 5 and not rospy.is_shutdown():
+    while not rospy.is_shutdown():
         if main_state == 0:
             main_state = spr.startSPR()
         elif main_state == 1:
